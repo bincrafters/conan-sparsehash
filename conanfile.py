79cb1de8397426a2f05db5d805557211034e7056
bincrafters-user <bincrafters@gmail.com>
2021-07-25 13:10:09 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.30.5

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class SparsehashConan(ConanFile):
     def build_requirements(self):
         if tools.os_info.is_windows and "CONAN_BASH_PATH" not in os.environ and \
                 tools.os_info.detect_windows_subsystem() != "msys2":
-            self.build_requires("msys2/20190524")
+            self.build_requires("msys2/cci.latest")
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
